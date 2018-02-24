19e81466e9b0d7b4ccbed1c250185df5d5444d56
zia <ziagaggoo@gmail.com>
2018-02-25 01:04:36 +0400

    Initial commit

---
+++ app/src/main/java/com/lovoo/android/tvmaze/App/TvMazeApplication.kt
@@ -0,0 +1,11 @@
+package com.lovoo.android.tvmaze.App
+
+import android.app.Application
+
+
+class TvMazeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/lovoo/android/tvmaze/constants/Configs.kt
@@ -0,0 +1,9 @@
+package com.lovoo.android.tvmaze.constants
+
+
+object Configs {
+
+    const val LOGTAG = "TvMazeApp"
+    const val BASE_URL = "http://api.tvmaze.com/"
+
+}
